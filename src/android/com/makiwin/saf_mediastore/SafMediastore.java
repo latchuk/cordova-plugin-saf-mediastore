cf5e8f3a57610267d1529fc2f93d80c89a2eb4c1
Marcos Latchuk <marcos_latchuk@hotmail.com>
2023-08-24 17:01:01 -0300

    agora vai

--- src/android/com/makiwin/saf_mediastore/SafMediastore.java
+++ src/android/com/makiwin/saf_mediastore/SafMediastore.java
@@ -299,12 +299,13 @@ public class SafMediastore extends CordovaPlugin implements ValueCallback<String
 							.openInputStream(uri);
 					ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
 				FileUtils.copy(inputStream, byteArrayOutputStream);
-				try (OutputStream outputStream = cordovaInterface.getContext().getContentResolver().openOutputStream(uri)) {
-					outputStream.write(byteArrayOutputStream.toByteArray());
-				} catch (Exception e2){
-					callbackContext.error(debugLog(e2));
-					return false;
-				}
+
+//				try (OutputStream outputStream = cordovaInterface.getContext().getContentResolver().openOutputStream(uri)) {
+//					outputStream.write(byteArrayOutputStream.toByteArray());
+//				} catch (Exception e2){
+//					callbackContext.error(debugLog(e2));
+//					return false;
+//				}
 
 				callbackContext.success(byteArrayOutputStream.toByteArray());
 				return true;
